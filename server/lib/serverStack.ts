424ee4d9270e20b787a22092fbad334c86d42138
aShunyaOtsuki <shunya.otsuki@access-company.com>
2022-08-25 18:14:31 +0900

    [server] imp hello world lambda (#2)

--- server/lib/serverStack.ts
+++ server/lib/serverStack.ts
@@ -1,16 +1,17 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
 
 export class ServerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ServerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const helloWorldLambda = new lambda.Function(this, "helloWorldLambda", {
+      functionName: "helloWorld",
+      code: lambda.Code.fromAsset("lambda/helloWorld"),
+      handler: "index.handler",
+      runtime: lambda.Runtime.NODEJS_16_X,
+    });
+    helloWorldLambda.addFunctionUrl();
   }
 }
