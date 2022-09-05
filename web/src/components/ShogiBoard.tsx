2ca150b83ff165ff3312c08ef481cca9eaf263a6
aShunyaOtuski <shunya.otsuki@access-company.com>
2022-09-05 18:51:32 +0900

    imp: initialShogiPlaces

--- web/src/components/ShogiBoard.tsx
+++ web/src/components/ShogiBoard.tsx
@@ -1,4 +1,5 @@
 import "../App.css";
+import { initialShogiPlaces } from "../data/shogi";
 
 export const ShogiBoard = () => {
   return (
@@ -6,7 +7,13 @@ export const ShogiBoard = () => {
       <div className="ShogiBoard">
         {[1, 2, 3, 4, 5].flatMap((i) => {
           return [1, 2, 3, 4, 5].map((j) => {
-            return <div className="KomaPlace">{`${i}${6 - j}`}</div>;
+            return (
+              <div className="KomaPlace">
+                {`${6 - j}${i}`}
+                <br />
+                {initialShogiPlaces[i - 1][j - 1]}
+              </div>
+            );
           });
         })}
       </div>
